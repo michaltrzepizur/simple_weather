880f628b90e30de8ef52cd636a7e73b6dacc59c3
Michal Trzepizur <stejnel@outlook.com>
2025-02-04 17:41:04 +0100

    add user city and temperature in home page

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -5,10 +5,14 @@ class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    // http://api.weatherapi.com/v1/current.json?key=93d44bc850fc4ecf926183736250202&q=Katowice&aqi=no
-    final response = await Dio().get(
-        'http://api.weatherapi.com/v1/current.json?key=93d44bc850fc4ecf926183736250202&q=Katowice&aqi=no');
-    print(response.data);
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=93d44bc850fc4ecf926183736250202&q=$city&aqi=no');
+    final responseData = response.data;
+    if (responseData == null) {
+      return null;
+    }
+    final name = responseData['location']['name'] as String;
+    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
+    return WeatherModel(city: name, temperature: temperature);
   }
 }
